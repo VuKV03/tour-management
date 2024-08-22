bc0036ed25dd2f818e11a080f85115a57cf0867e
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-08-22 11:28:28 +0700

    build: mvc

--- index.ts
+++ index.ts
@@ -1,11 +1,8 @@
-import express, { Express, Request, Response } from "express";
-import sequelize from "./config/database";
+import express, { Express } from "express";
 import dotenv from "dotenv";
-import Tour from "./models/tour.model";
+import clientRoutes from "./routes/client/index.route";
 
 dotenv.config();
-
-sequelize;
 
 const app: Express = express();
 const port: (number | string) = `${process.env.PORT}` || 3000;
@@ -13,16 +10,8 @@ const port: (number | string) = `${process.env.PORT}` || 3000;
 app.set("views", "./views");
 app.set("view engine", "pug");
 
-app.get("/tours", async (req: Request, res: Response) => {
-  const tours = await Tour.findAll({
-    raw: true,
-  });
-
-  res.render("client/pages/tours/index", {
-    pageTitle: "Danh sách tour",
-    tours: tours
-  });
-});
+//Client Routes
+clientRoutes(app);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
