8d406568211e70f33c02fcf18e07906ade6bb44f
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-08-22 10:18:18 +0700

    build: list tours

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import sequelize from "./config/database";
 import dotenv from "dotenv";
+import Tour from "./models/tour.model";
 
 dotenv.config();
 
@@ -12,8 +13,15 @@ const port: (number | string) = `${process.env.PORT}` || 3000;
 app.set("views", "./views");
 app.set("view engine", "pug");
 
-app.get("/tours", (req: Request, res: Response) => {
-  res.render("client/pages/tours/index");
+app.get("/tours", async (req: Request, res: Response) => {
+  const tours = await Tour.findAll({
+    raw: true,
+  });
+
+  res.render("client/pages/tours/index", {
+    pageTitle: "Danh sách tour",
+    tours: tours
+  });
 });
 
 app.listen(port, () => {
