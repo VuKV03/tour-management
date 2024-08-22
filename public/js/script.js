2608f6f2475bd21eea068c4d90ac0cba17e5ddb1
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-08-22 16:03:52 +0700

    build: cart

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,6 @@
+// Slider Tour Detail
 const sliderMain = document.querySelector(".sliderMain");
-if(sliderMain) {
+if (sliderMain) {
   const swiper = new Swiper(".sliderMain", {
     pagination: {
       el: ".swiper-pagination",
@@ -7,3 +8,43 @@ if(sliderMain) {
     },
   });
 }
+// End Slider Tour Detail
+
+// Carts
+
+// Tạo giỏ hàng mới nếu chưa có
+const cart = localStorage.getItem("cart");
+if (!cart) {
+  localStorage.setItem("cart", JSON.stringify([]));
+}
+
+// Thêm tour vào cart
+const formAddToCart = document.querySelector("[form-add-to-cart]");
+if (formAddToCart) {
+  formAddToCart.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const quantity = parseInt(event.target.elements.quantity.value);
+    const tourId = parseInt(formAddToCart.getAttribute("tour-id"));
+
+    if (quantity > 0 && tourId) {
+      const cart = JSON.parse(localStorage.getItem("cart"));
+
+      const indexExistTour = cart.findIndex((item) => item.tourId == tourId);
+
+      if (indexExistTour == -1) {
+        cart.push({
+          tourId: tourId,
+          quantity: quantity,
+        });
+      } else {
+        cart[indexExistTour].quantity =
+          cart[indexExistTour].quantity + quantity;
+      }
+
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  });
+}
+
+// End Carts
