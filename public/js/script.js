37306eba31935e2fc9548d41ce8926856342bf15
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-08-22 15:38:19 +0700

    build: slider

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,9 @@
-console.log("siuu");
+const sliderMain = document.querySelector(".sliderMain");
+if(sliderMain) {
+  const swiper = new Swiper(".sliderMain", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+}
