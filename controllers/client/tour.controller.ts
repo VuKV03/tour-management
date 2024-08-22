fc71f57f39f3228a060cf2323f75770b020d077b
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-08-22 15:23:18 +0700

    build: detail tour

--- controllers/client/tour.controller.ts
+++ controllers/client/tour.controller.ts
@@ -1,12 +1,14 @@
 import { Request, Response } from "express";
-import Tour from "../../models/tour.model";
 import sequelize from "../../config/database";
 import { QueryTypes } from "sequelize";
+import Tour from "../../models/tour.model";
 
+// [GET] /tours/:slugCategory
 export const index = async (req: Request, res: Response) => {
   const slugCategory = req.params.slugCategory;
 
-  const tours = await sequelize.query(`
+  const tours = await sequelize.query(
+    `
     SELECT tours.*, ROUND(price * (1 - discount/100), 0) AS price_special
     FROM tours
     JOIN tours_categories ON tours.id = tours_categories.tour_id
@@ -17,22 +19,47 @@ export const index = async (req: Request, res: Response) => {
       AND categories.status = 'active'
       AND tours.deleted = false
       AND tours.status = 'active';
-  `, {
-    type: QueryTypes.SELECT,
-  });
+  `,
+    {
+      type: QueryTypes.SELECT,
+    }
+  );
 
-  tours.forEach(tour => {
-    if(tour["images"]) {
+  tours.forEach((tour) => {
+    if (tour["images"]) {
       const images = JSON.parse(tour["images"]);
       tour["image"] = images[0];
     }
     tour["price_special"] = +tour["price_special"];
-  })
-
+  });
 
   res.render("client/pages/tours/index", {
     pageTitle: "Danh sách tour",
-    tours: tours
+    tours: tours,
   });
-  
-}
+};
+
+// [GET] /tours/detail/:slugTour
+export const detail = async (req: Request, res: Response) => {
+  const slugTour = req.params.slugTour;
+
+  const tourDetail = await Tour.findOne({
+    where: {
+      slug: slugTour,
+      deleted: false,
+      status: "active"
+    },
+    raw: true
+  });
+
+  if(tourDetail["images"]) {
+    tourDetail["images"] = JSON.parse(tourDetail["images"]);
+  }
+
+  tourDetail["price_special"] = (tourDetail["price"] * (1 - tourDetail["discount"]/100));
+
+  res.render("client/pages/tours/detail", {
+    pageTitle: "Chi tiết tour",
+    tourDetail: tourDetail
+  });
+};
